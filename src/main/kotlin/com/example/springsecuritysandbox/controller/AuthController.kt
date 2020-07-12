096a1490fa5cefc9d608189c0c8fbd3eb892de94
yuizho <yuizho3@gmail.com>
2020-07-12 18:37:24 +0900

    modify loggedin screen to show user information by SecurityContext

--- src/main/kotlin/com/example/springsecuritysandbox/controller/AuthController.kt
+++ src/main/kotlin/com/example/springsecuritysandbox/controller/AuthController.kt
@@ -1,13 +1,19 @@
 package com.example.springsecuritysandbox.controller
 
+import com.example.springsecuritysandbox.domain.User
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
 class AuthController {
 
     @GetMapping("/loggedin")
-    fun index(): String = "auth/index"
-
+    fun index(model: Model): String {
+        // SecurityContextHolderはthread localにユーザの認証情報を保存している
+        val authentication = SecurityContextHolder.getContext().authentication
+        model.addAttribute("user", authentication.principal as User)
+        return "auth/loggedin"
+    }
 }
